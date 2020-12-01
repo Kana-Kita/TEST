406112492679d4aae80278a856d02e3664807e87
Kana-Kita <k-kita@techno.khi.co.jp>
2020-12-01 13:03:38 +0900

    コンソールアプリ追加

---
+++ TestApp1/ConsoleApp1/ConsoleApp1/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+
+        For i As Integer = 0 To 10
+            Console.WriteLine("カウント： " & i)
+        Next
+    End Sub
+End Module
