f427b7ec70f12118eb05e7777b3fcd1497614a87
Kana-Kita <k-kita@techno.khi.co.jp>
2020-12-04 13:18:50 +0900

    プログラム変更

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -1,11 +1,11 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
+    Sub Main()
         Console.WriteLine("Hello World!")
 
-        For i As Integer = 0 To 10
-            Console.WriteLine("カウント：" & i)
+        For i As Integer = 0 To 10 Step 1
+            Console.WriteLine("カウント：" & i.ToString())
         Next
     End Sub
 End Module
