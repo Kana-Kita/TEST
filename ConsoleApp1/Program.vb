e951298ecd8fa2b7be93b9ed67cbc0830cb0d1d0
Kana-Kita <k-kita@techno.khi.co.jp>
2020-12-04 13:20:53 +0900

    調整2

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -4,7 +4,7 @@ Module Program
     Sub Main()
         Console.WriteLine("Hello World!")
 
-        For i As Integer = 0 To 10 Step 1
+        For i As Integer = 0 To 10
             Console.WriteLine("カウント：" & i.ToString())
         Next
     End Sub
