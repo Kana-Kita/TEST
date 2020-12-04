4f8c736297c56c61cff2f8cb5efd9b162d98ff81
Kana-Kita <k-kita@techno.khi.co.jp>
2020-12-04 13:35:46 +0900

    C#に変更

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("テスト");
+            }
+        }
+    }
+}
